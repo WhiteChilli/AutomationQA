edf1a6a25531de4b7f3ddd5e693a29fe485f097f
alocmere <alocmere@quintessence.net>
2023-02-03 15:02:21 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Today is a good day" + " " + "!");
+        System.out.println("I want to debug this code - 1");
+        System.out.println("I want to debug this code - 2");
+        System.out.print("code same line");
+        System.out.print("code same line");
+    }
+}
