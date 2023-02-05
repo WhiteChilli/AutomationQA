2dfa44fbef3690003c1aae7348562bf3a403f304
alocmere <alocmere@quintessence.net>
2023-02-05 14:41:04 -0300

    Homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Lilac");
+        System.out.println("Purple");
+        System.out.println("Blue");
 
-        System.out.println("Hello world!");
-        System.out.println("Today is a good day" + " " + "!");
-        System.out.println("I want to debug this code - 1");
-        System.out.println("I want to debug this code - 2");
-        System.out.print("code same line");
-        System.out.print("code same line");
     }
 }
---
+++ src/Star.java
@@ -0,0 +1,21 @@
+public class Star {
+
+    public static void main(String[] args) {
+
+        System.out.println("________________✯__________________");
+        System.out.println("______________✯✯✯✯________________");
+        System.out.println("____________✯✯✯__✯✯✯_____________");
+        System.out.println("___________✯✯✯____✯✯✯____________");
+        System.out.println("_____✯✯✯✯✯✯✯______✯✯✯✯✯✯✯_____");
+        System.out.println("______✯✯✯______________✯✯✯_______");
+        System.out.println("_________✯✯____________✯✯_________");
+        System.out.println("______✯✯✯______________✯✯✯_______");
+        System.out.println("_____✯✯✯✯✯✯✯______✯✯✯✯✯✯✯_____");
+        System.out.println("__________✯✯✯_____✯✯✯____________");
+        System.out.println("____________✯✯✯__✯✯✯_____________");
+        System.out.println("______________✯✯✯✯________________");
+        System.out.println("________________✯__________________");
+
+
+    }
+}
